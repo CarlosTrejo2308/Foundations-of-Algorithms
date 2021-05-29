11ad9baf94aba671455f2ad747326a16c12df2dc
Carlos N. Trejo <cntrejo@prodigy.net-mx>
2021-05-29 01:43:59 -0500

    Add comparisions

--- src/Introduction/arraySearch.go
+++ src/Introduction/arraySearch.go
@@ -6,7 +6,10 @@ import (
 	"strconv"
 )
 
-func BinSearch(S []int, x int, location *int) {
+func BinSearch(S []int, x int, location *int) int {
+
+	comparisons := 0
+
 	high := len(S)
 
 	var low, mid int
@@ -14,6 +17,7 @@ func BinSearch(S []int, x int, location *int) {
 	*location = -1
 
 	for low <= high && *location == -1 {
+		comparisons++
 		mid = int(math.Floor(float64(low+high) / float64(2.0)))
 
 		if x == S[mid] {
@@ -24,12 +28,13 @@ func BinSearch(S []int, x int, location *int) {
 			low = mid + 1
 		}
 	}
+
+	return comparisons
 }
 
-func mainBinSearch() {
-	// Default variables
-	array := []int{3, 4, 8, 9, 12, 24, 27, 70, 80, 81, 82, 83, 90, 100}
+var array = []int{3, 4, 8, 9, 12, 24, 27, 70, 80, 81, 82, 83, 90, 100}
 
+func mainBinSearch() {
 	var location int
 
 	// Get number to search
@@ -38,14 +43,15 @@ func mainBinSearch() {
 	x, _ := strconv.Atoi(userInput)
 
 	// Search
-	BinSearch(array, x, &location)
+	comp := BinSearch(array, x, &location)
 
 	// Print the result
 	if location < 0 {
-		fmt.Printf("The number %d was not in the list\n", x)
+		fmt.Printf("The number %d was not in the list", x)
 	} else {
-		fmt.Printf("The number %d is in the index %d\n", x, location)
+		fmt.Printf("The number %d is in the index %d", x, location)
 	}
+	fmt.Printf(" with %d comparisionan\n", comp)
 
 }
 
@@ -54,11 +60,14 @@ func mainBinSearch() {
 // S, array to search
 // x, number to find
 // location, index to save the index
-func SequentialSearch(n int, S []int, x int, location *int) {
+func SequentialSearch(n int, S []int, x int, location *int) int {
+	comparisons := 0
+
 	*location = 0
 
 	// Keep looking until we find the elemnt or we run the whole array
 	for *location < n && S[*location] != x {
+		comparisons++
 		*location++
 	}
 
@@ -66,11 +75,10 @@ func SequentialSearch(n int, S []int, x int, location *int) {
 	if *location >= n {
 		*location = -1
 	}
+	return comparisons
 }
 
 func mainSequential() {
-	// Default variables
-	array := []int{3, 4, 8, 9, 12, 24, 27, 70}
 	n := cap(array)
 	var location int
 
@@ -80,13 +88,14 @@ func mainSequential() {
 	x, _ := strconv.Atoi(userInput)
 
 	// Search
-	SequentialSearch(n, array, x, &location)
+	comp := SequentialSearch(n, array, x, &location)
 
 	// Print the result
 	if location < 0 {
-		fmt.Printf("The number %d was not in the list\n", x)
+		fmt.Printf("The number %d was not in the list", x)
 	} else {
-		fmt.Printf("The number %d is in the index %d\n", x, location)
+		fmt.Printf("The number %d is in the index %d", x, location)
 	}
+	fmt.Printf(" with %d comparisionan\n", comp)
 
 }
