83195a3c34d109e0bd2b95e15fd95a4e074cf0f4
Carlos N. Trejo <cntrejo@prodigy.net-mx>
2021-05-29 00:43:33 -0500

    Move matriz multplication and made functions public

--- src/Introduction/arrays.go
+++ src/Introduction/arrays.go
@@ -21,3 +21,15 @@ func exchangeSort(n int, S []int) {
 	}
 
 }
+
+func matrixMultiplication(n int, A, B, C [][]int) {
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			C[i][j] = 0
+			for k := 0; k < n; k++ {
+				C[i][j] = C[i][j] + A[i][k]*B[k][j]
+			}
+		}
+	}
+}
