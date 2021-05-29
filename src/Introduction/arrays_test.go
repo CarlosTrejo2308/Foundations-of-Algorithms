8c29c5574f2b03752e9cde111f9bb663a17e2a3c
Carlos N. Trejo <cntrejo@prodigy.net-mx>
2021-05-28 23:15:26 -0500

    Add test for addArray

---
+++ src/Introduction/arrays_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestAddArray(t *testing.T) {
+	want := 15
+	n := 5
+	array1 := []int{1, 2, 3, 4, 5}
+
+	got := addArray(n, array1)
+
+	if got != want {
+		t.Errorf("addArray(n, {1, 2, 3, 4, 5}). Got %d Want %d", got, want)
+	}
+}
